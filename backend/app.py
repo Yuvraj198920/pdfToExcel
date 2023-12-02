7b8e94da68e7cc5741e5f85bc83402abef099694
Yuvraj198920 <yuvvraaj0flying@gmail.com>
2023-12-02 15:34:27 +0530

    Upload file and save to uploads folder. Also can view data in browser

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, flash, redirect, url_for
 from werkzeug.utils import secure_filename
 import os
 
+from pdfminer.high_level import extract_text
 
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'pdf'}
@@ -11,6 +12,9 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_files(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def extract_text_from_pdf(pdf_path):
+    return extract_text(pdf_path)
 
 @app.route('/')
 def hello_world():
@@ -23,8 +27,12 @@ def upload_file():
         file = request.files.get('file')
         if file and allowed_files(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return 'File uploaded successfully'
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            extracted_text = extract_text_from_pdf(file_path)
+            print(extract_text)
+            return '<pre>' + extracted_text + '</pre>'
+
         else:
             return 'No valid file provided', 400
     return '''
